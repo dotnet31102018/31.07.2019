246ae45043b5d8ed7114f2ae66411089084f767c
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-07-31 20:36:47 +0300

    Add files via upload

---
+++ ValueConverter/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3107_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Person obj { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            obj = new Person() { Name = "hi im person" };
+            this.DataContext = this;
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"Person name {Name}";
+            }
+        }
+    }
+}
---
+++ ValueConverter/ValueConverterOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _3107_
+{
+    public class ValueConverterOne : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isenable = true;
+            if (string.IsNullOrEmpty(value.ToString()))
+            {
+                isenable = false;
+            }
+            return isenable;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
