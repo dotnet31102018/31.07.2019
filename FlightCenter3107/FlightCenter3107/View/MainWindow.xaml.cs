f30c0a45c54fc3df36e0b1ba34ea2919472462e5
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-08-01 23:17:33 +0300

    Add files via upload

---
+++ FlightCenter3107/FlightCenter3107/View/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FligthCenterCore;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightCenter3107
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private MainWindowViewModel vm = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+
+   
+
+    }
+}
---
+++ FlightCenter3107/FlightCenter3107/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using FligthCenterCore;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlightCenter3107
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public DelegateCommand BuyCommand { get; set; }
+
+        public DelegateCommand SearchFlightCommand { get; set; }
+
+        public int FlightNumber { get; set; }
+
+        public Flight MyFlight { get; set; }
+
+        private AnonymouseFacade facade;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            FlightNumber = 0;
+
+            facade = FlightCenterSystem.GetInstacne().GetAnonymousFacade();
+
+            BuyCommand = new DelegateCommand(() => { MessageBox.Show("Buying ticket! yeah!"); }, () => { return MyFlight != null && MyFlight.Vacancy > 0; });
+
+            SearchFlightCommand = new DelegateCommand(() => {
+                MyFlight = facade.GetFlightById(FlightNumber);
+
+                PropertyChanged(this, new PropertyChangedEventArgs("MyFlight"));
+
+                BuyCommand.RaiseCanExecuteChanged();
+            }, () => { return true; });
+        }
+
+    }
+}
